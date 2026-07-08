20f5c3b8fe0608fdffb2b8bcfbba14f9ea3c782d
Jordan Harband <ljharb@gmail.com>
2026-07-07 21:16:11 -0700

    [Refactor] use `Array.prototype.with` rather than mutating

--- src/promise-series.js
+++ src/promise-series.js
@@ -1,11 +1,14 @@
+'use strict';
+
+const arrayWith = require('array.prototype.with');
+
 function promiseSeries (arr, cb) {
   var count = 0
   return arr.reduce(function (acc, action) {
     return acc.then(function (arrActive) {
       return Promise.resolve(cb(action, count, arrActive)).then(function (value) {
-        arrActive[count] = value
-        count = count + 1
-        return arrActive
+        count += 1
+        return arrayWith(arrActive, count - 1, value);
       })
     })
   }, Promise.resolve(arr))
